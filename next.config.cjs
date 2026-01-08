9f2d35cc6995fc161fadfbf52e1c2e8d7ca6f99c
summerhasuna <summer.h@optimaxeyewear.com>
2026-01-08 18:57:24 +0200

    updateit

--- next.config.cjs
+++
@@ -1,7 +0,0 @@
-// CommonJS wrapper for Next config to avoid ESM/CommonJS mismatch when "type": "module" is set in package.json
-// Keep this file minimal and in sync with next.config.ts
-
-module.exports = {
-  /* config options here */
-  reactCompiler: true,
-};
