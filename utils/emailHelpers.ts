cae0b35fdadda5801058b98b3957ca3b2ef32e03
Nati Hadad <nati.h@optimaxeyewear.com>
2026-06-11 14:16:28 +0300

    updated the exam link if-statement

--- utils/emailHelpers.ts
+++ utils/emailHelpers.ts
@@ -39,15 +39,15 @@ export const generateHTML = (config: Config): string => {
           </td></tr>`;
         case "examlink":
           return `
-          {% if exam %}
           <tr><td style="padding: 0 20px 16px 20px;">
-            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid rgba(0, 0, 0, 0.12); border-radius: 4px; background-color: #ffffff; border-collapse: separate;">
-              <tr>
-                <td width="4" style="background-color: ${store.calloutBorderColor}; border-radius: 4px 0 0 4px; font-size: 0; line-height: 0;">&nbsp;</td>
-                <td style="padding: 16px;">
-                  <div style="font-size: 16px; font-weight: 400; line-height: 24px; color: ${store.textColor};">
-                    ${section.text || ""}
-                  </div>
+          <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid rgba(0, 0, 0, 0.12); border-radius: 4px; background-color: #ffffff; border-collapse: separate;">
+          <tr>
+          <td width="4" style="background-color: ${store.calloutBorderColor}; border-radius: 4px 0 0 4px; font-size: 0; line-height: 0;">&nbsp;</td>
+          <td style="padding: 16px;">
+          <div style="font-size: 16px; font-weight: 400; line-height: 24px; color: ${store.textColor};">
+          ${section.text || ""}
+          </div>
+          {% if exam %}
                   <div style="padding-top: 16px;">
                     <a href={{ exam.link }} style="background-color: ${store.buttonBgColor}; color: ${store.buttonTextColor}; padding: 0px 20px;height:32px; line-height:32px; text-align: center; border-radius: 20px; font-size: 14px; font-weight: 700; text-decoration: none; display: inline-block;">Start Test  </a>
                   </div>
