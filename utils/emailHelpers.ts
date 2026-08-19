86d53275d5bd4ab48d2793a9252e71b628777dfc
Nati Hadad <nati.h@optimaxeyewear.com>
2026-08-19 14:52:44 +0300

    updated tool

--- utils/emailHelpers.ts
+++ utils/emailHelpers.ts
@@ -53,7 +53,7 @@ export const generateHTML = (config: Config): string => {
                            style="background-color: #020621; color: #ffffff; padding: 0px 20px; height:32px;
                               line-height:32px; text-align: center; border-radius: 20px; font-size: 14px;
                               font-weight: 700; text-decoration: none; display: inline-block;">
-                            Start Test!!!!!
+                            Start Test
                         </a>
                         </div>
                         </td>
